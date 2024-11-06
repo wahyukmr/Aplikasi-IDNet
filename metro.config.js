cd51092a0a34d562be5913153954a0b48d6b7ab5
wahyukmr <maryu9229@gmail.com>
2024-11-06 12:12:36 +0700

    [add] ➕instalation and configuration of main libraries

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {withNativeWind} = require('nativewind/metro');
 
 /**
  * Metro configuration
@@ -9,3 +10,5 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+module.exports = withNativeWind(config, {input: './src/styles/global.css'});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#E05297',
+        secondary: '#F3BAD6',
+      },
+    },
+  },
+  plugins: [],
+};
