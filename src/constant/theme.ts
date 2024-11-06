1e0ae685c5cda0809fa84edbe8dd93928b2f3206
wahyukmr <maryu9229@gmail.com>
2024-11-06 15:28:57 +0700

    [add] ➕adding theme colors

---
+++ src/constant/theme.ts
@@ -0,0 +1,33 @@
+export const lightThemes = {
+  barStyle: 'dark-content',
+  hairLine: 'lightgrey',
+  error: '#e84e4f',
+  primary100: '#E05297',
+  primary200: '#F3BAD6',
+  primary300: '#E0E2E5',
+  accent100: '#f5be0b',
+  accent200: '#fef1c7',
+  text100: '#4b0c24',
+  text200: '#333333',
+  text300: '#78716c',
+  bg100: '#FCEEF5',
+  bg200: '#ffffff',
+  bg300: '#FAD9E6',
+};
+
+export const darkThemes = {
+  barStyle: 'light-content',
+  hairLine: 'darkgrey',
+  error: '#e84e4f',
+  primary100: '#E05297',
+  primary200: '#F3BAD6',
+  primary300: '#E0E2E5',
+  accent100: '#f5be0b',
+  accent200: '#fef1c7',
+  text100: '#fcf3f8',
+  text200: '#cccccc',
+  text300: '#e0e0e0',
+  bg100: '#0F1626',
+  bg200: '#1e2436',
+  bg300: '#363c4e',
+};
